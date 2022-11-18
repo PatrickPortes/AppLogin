47eb650bca253b766ece35259cb78d4ac4d8b55c
Patrick Portes <89034086+PatrickPortes@users.noreply.github.com>
2022-11-18 18:48:47 -0300

    Activities e UI
    Aprimorando o Design (UI), criando e desenvolvendo suas Activities(Splash, Login, Register, Home).

---
+++ app/src/main/java/com/example/applogin/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.applogin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar!!.hide()
+
+        Handler().postDelayed({
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+        }, 3000)
+        
+    }
+
+}
