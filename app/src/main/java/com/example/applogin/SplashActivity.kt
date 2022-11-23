0626c0c3bb38b66e341084fc8ed8cfe462ace310
Patrick Portes <89034086+PatrickPortes@users.noreply.github.com>
2022-11-22 22:39:09 -0300

    Design UI RegisterActivity
    A layout_register foi desenvolvida e configurada todo o design de seus componentes, foram feitas correções (toolbar.hide em alguns layouts, método finish na splash, e alterado nomes de algumas views).

--- app/src/main/java/com/example/applogin/SplashActivity.kt
+++ app/src/main/java/com/example/applogin/SplashActivity.kt
@@ -16,6 +16,7 @@ class SplashActivity : AppCompatActivity() {
 
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
+            finish()
 
         }, 3000)
         
