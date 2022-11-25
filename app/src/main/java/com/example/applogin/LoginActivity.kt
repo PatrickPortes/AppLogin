0d876b1a00f865ee2b9ddb48d0462f2440256350
Patrick Portes <89034086+PatrickPortes@users.noreply.github.com>
2022-11-24 23:27:12 -0300

    Propriedades e Métodos Login e Register
    Desenvolvendo propriedades e métodos das activity register e login, inicializando suas respectivas views e implementando os métodos onClick das interfaces, e foi feita uma alteração  nos botões de acesso de interface para sign up e sign in

--- app/src/main/java/com/example/applogin/LoginActivity.kt
+++ app/src/main/java/com/example/applogin/LoginActivity.kt
@@ -1,13 +1,44 @@
 package com.example.applogin
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btLogin: Button
+    private lateinit var btSignUp: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         supportActionBar!!.hide()
+
+        startingComponents()
+
+        btLogin.setOnClickListener(){
+            Toast.makeText(this, "Success Login!!!", Toast.LENGTH_SHORT).show()
+        }
+
+        btSignUp.setOnClickListener(){
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun startingComponents(){
+        etEmail = editTextEmailLogin
+        etPassword = editTextPasswordLogin
+        btLogin = buttonLogin
+        btSignUp = textViewSignUp
     }
 }
+
--- app/src/main/java/com/example/applogin/RegisterActivity.kt
+++ app/src/main/java/com/example/applogin/RegisterActivity.kt
@@ -1,13 +1,44 @@
 package com.example.applogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var etName: EditText
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btRegister: Button
+    private lateinit var btSignIn: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
         supportActionBar!!.hide()
+
+        startingComponents()
+
+        btRegister.setOnClickListener(){
+            Toast.makeText(this, "Success Register!!!", Toast.LENGTH_SHORT).show()
+        }
+
+        btSignIn.setOnClickListener(){
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun startingComponents(){
+        etEmail = editTextEmailRegister
+        etPassword = editTextPasswordRegister
+        btRegister = buttonRegister
+        btSignIn = textViewSignIn
     }
 }
